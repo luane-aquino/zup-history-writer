efa0ca378cce61e946bb64f815ef503a58014a49
Luane Aquino <luanecaroline91@gmail.com>
2020-04-21 18:21:22 -0300

    testing db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,8 +8,26 @@ const sequelize = new Sequelize('writerdb', 'root', '1234', {
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully.')
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    console.error('Unable to connect to the database:', err)
+  })
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+})
+
+// force: true will drop the table if it already exists
+User.sync({ force: true }).then(() => {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  })
+})
