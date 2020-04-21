3b3ceed37d41844c2a84f4ebeec76c522e9917b1
Luane Aquino <luanecaroline91@gmail.com>
2020-04-21 09:47:03 -0300

    update readme

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.get('/', function (req, res) {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.post('/story', (req, res) => {
+  const story = req.body
+  res.status(200).json({ message: "successfull" })
+})
+
+app.post('/comment', (req, res) => {
+  const comment = req.body
+  res.status(200).json({ message: "successfull" })
 })
 
 app.listen(3000)
