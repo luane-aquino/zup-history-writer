8a10f2a766833492eb4ab648026e945434dc05c5
Luane Aquino <luanecaroline91@gmail.com>
2020-04-26 12:56:43 -0300

    implement can consume all comments

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,12 +1,22 @@
-const comment = document.getElementById('comment')
+const commentList = document.getElementById('comment__list__container')
 
 axios.get('http://localhost:3000/story/1/comments')
-  .then(function (response) {
-    // handle success
-    console.log('**', response.data[0].comment)
-    comment.innerHTML = `${response.data[0].comment}`
+  .then(response => {
+    let allComments = ''
+    for (const item of response.data) {
+      allComments += `
+      <li>
+        <img src="./images/yoda-profile.jpg" alt="yoda">
+        <p>${item.comment}</p>
+        <div>
+          <button class="btn green">Ajudou</button>
+          <button class="btn red btn--larger">Não ajudou</button>
+        </div>
+      </li>
+      `
+    }
+    commentList.innerHTML = allComments
   })
   .catch(function (error) {
-    // handle error
     console.log(error)
   })
