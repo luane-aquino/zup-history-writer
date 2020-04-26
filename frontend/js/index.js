4cbc6bddcc8c03bef869132c2e068d76618a2f08
Luane Aquino <luanecaroline91@gmail.com>
2020-04-26 15:35:35 -0300

    implement user can post a comment

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,6 @@
 const commentList = document.getElementById('comment__list__container')
+const commentBtn = document.getElementById('comment-btn')
+const commentTextarea = document.getElementById('comment-textarea')
 
 axios.get('http://localhost:3000/story/1/comments')
   .then(response => {
@@ -20,3 +22,16 @@ axios.get('http://localhost:3000/story/1/comments')
   .catch(function (error) {
     console.log(error)
   })
+
+commentBtn.addEventListener('click', () => {
+  console.log('****')
+  axios.post('http://localhost:3000/story/1/comment', {
+    comment: commentTextarea.value
+  })
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+})
