c40fde21dd02ed7dfc2a90893dd7d102f535a802
Luane Aquino <luanecaroline91@gmail.com>
2020-04-27 15:17:43 -0300

    implements endpoint /story/:id

--- backend/src/routes/story.js
+++ backend/src/routes/story.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/StoryController')
 
+router.get('/:id', controller.getStoryById)
+
 router.post('/:id/comment', controller.createComment)
 
 router.get('/:id/comments', controller.getComments)
--- frontend/js/story.js
+++ frontend/js/story.js
@@ -3,13 +3,12 @@ const storyTitle = document.getElementById('story-title')
 const storySubtitle = document.getElementById('story-subtitle')
 const storyAuthor = document.getElementById('story-author')
 
-// TODO: add endpoint /story/:id
 function getStory() {
-  axios.get('http://localhost:3000/user/1/stories')
+  axios.get('http://localhost:3000/story/3')
     .then(response => {
-      storyTitle.innerHTML = response.data[2].title
-      storySubtitle.innerHTML = response.data[2].subtitle
-      storyText.innerHTML = response.data[2].text
+      storyTitle.innerHTML = response.data.title
+      storySubtitle.innerHTML = response.data.subtitle
+      storyText.innerHTML = response.data.text
     })
     .catch(function (error) {
       console.log(error)
