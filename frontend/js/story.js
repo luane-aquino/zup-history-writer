7c13ddc78aa033d2ad2a088203207da314c84830
Luane Aquino <luanecaroline91@gmail.com>
2020-04-27 15:01:41 -0300

    implements get story author

--- frontend/js/story.js
+++ frontend/js/story.js
@@ -1,7 +1,9 @@
 const storyText = document.getElementById('story-text')
 const storyTitle = document.getElementById('story-title')
 const storySubtitle = document.getElementById('story-subtitle')
+const storyAuthor = document.getElementById('story-author')
 
+// TODO: add endpoint /story/:id
 function getStory() {
   axios.get('http://localhost:3000/user/1/stories')
     .then(response => {
@@ -14,6 +16,17 @@ function getStory() {
     })
 }
 
+function getUser() {
+  axios.get('http://localhost:3000/user/1')
+    .then(response => {
+      storyAuthor.innerHTML = response.data.name
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
 window.addEventListener('DOMContentLoaded', () => {
+  getUser()
   getStory()
 })
