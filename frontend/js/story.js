1a3ce56201d01bd6f4c1adfa453a7c7dc66d0668
Luane Aquino <luanecaroline91@gmail.com>
2020-04-29 14:45:08 -0300

    implements consume tags in frontend

--- frontend/js/story.js
+++ frontend/js/story.js
@@ -2,13 +2,15 @@ const storyText = document.getElementById('story-text')
 const storyTitle = document.getElementById('story-title')
 const storySubtitle = document.getElementById('story-subtitle')
 const storyAuthor = document.getElementById('story-author')
+const storyTags = document.getElementById('story-tags-container')
 
 function getStory() {
-  axios.get('http://localhost:3000/story/3')
+  axios.get('http://localhost:3000/story/4')
     .then(response => {
       storyTitle.innerHTML = response.data.title
       storySubtitle.innerHTML = response.data.subtitle
       storyText.innerHTML = response.data.text
+      addTagsToUI(response.data.tags)
     })
     .catch(function (error) {
       console.log(error)
@@ -23,6 +25,15 @@ function getUser() {
     .catch(function (error) {
       console.log(error)
     })
+}
+
+function addTagsToUI(tags) {
+  const tagsArr = tags.split(',')
+  tagsArr.forEach(tag => {
+    const li = document.createElement('li')
+    li.innerHTML = tag
+    storyTags.appendChild(li)
+  })
 }
 
 window.addEventListener('DOMContentLoaded', () => {
