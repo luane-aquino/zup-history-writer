a65002ff58ea11e637d3286098388f984b52db32
Luane Aquino <luanecaroline91@gmail.com>
2020-04-29 09:40:36 -0300

    implements tag input field in frontend

--- frontend/js/create-story.js
+++ frontend/js/create-story.js
@@ -3,6 +3,10 @@ const title = document.getElementById('title')
 const subtitle = document.getElementById('subtitle')
 const story = document.getElementById('story')
 const tags = document.getElementById('tags')
+// tag input field
+const tagContainer = document.querySelector('.tag-container')
+const tagInput = document.querySelector('.tag-container input')
+let tagsArr = []
 
 sendStoryBtn.addEventListener('click', () => {
   console.log('hey, you clicked me!')
@@ -20,4 +24,51 @@ sendStoryBtn.addEventListener('click', () => {
   }).catch(function (error) {
     console.log(error)
   })
+})
+
+/* tag input field */
+function createTag(label) {
+  const div = document.createElement('div')
+  div.setAttribute('class', 'tag')
+  const span = document.createElement('span')
+  span.innerHTML = label
+  const closeBtn = document.createElement('i')
+  closeBtn.setAttribute('class', 'material-icons')
+  closeBtn.setAttribute('data-item', label)
+  closeBtn.innerHTML = 'close'
+
+  div.appendChild(span)
+  div.appendChild(closeBtn)
+  return div
+}
+
+function reset() {
+  document.querySelectorAll('.tag').forEach(tag => {
+    tag.parentElement.removeChild(tag)
+  })
+}
+
+function addTags() {
+  reset()
+  tagsArr.slice().reverse().forEach(tag => {
+    const newElem = createTag(tag)
+    tagContainer.prepend(newElem)
+  })
+}
+
+tagInput.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    tagsArr.push(tagInput.value)
+    addTags()
+    tagInput.value = ''
+  }
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'I') {
+    const value = e.target.getAttribute('data-item')
+    const index = tagsArr.indexOf(value)
+    tagsArr = [...tagsArr.slice(0, index), ...tagsArr.slice(index + 1)]
+    addTags()
+  }
 })
